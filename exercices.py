c7fb73fc259bc05f102e1f89b8d831bcf57dd653
BlackDrift <fovet.vianney@gmail.com>
2022-11-07 11:34:16 +0100

    adding exercices.py

---
+++ exercices.py
@@ -0,0 +1,36 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+def modulo(x,y):
+    return x%y
+
+def calcul_r_par_s(salaire_h, h_jo, jo_an) :
+    return ((salaire_h*h_jo) * jo_an) / 3600
+
+DEBUT
+ DEFINIR add(x, y) :
+    RENVOYER x + y
+
+ DEFINIR sub(x, y) :
+    RENVOYER x - y
+
+ DEFINIR multiply(x, y) :
+    RENVOYER x * y
+
+ DEFINIR divide(x, y) :
+    RENVOYER x / y
+
+ DEFINIR modulo(x, y) :
+    RENVOYER x % y
+
+
+FIN
