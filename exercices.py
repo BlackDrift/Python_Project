d718809e36d51a6ffbbd322a7e27f3957a9cd923
BlackDrift <fovet.vianney@gmail.com>
2022-11-08 09:11:37 +0100

    exercice correction

--- exercices.py
+++ exercices.py
@@ -8,14 +8,33 @@ def multiply(x,y):
     return x*y
 
 def divide(x,y):
-    return x/y
+    #si y est différent de 0, alors on peut faire la division
+    if y != 0 :
+        #on calcule donc la division de x par y
+        return x/y
+    #sinon, bah diviser par 0 c'est le mal donc non en fait
+    else :
+        #du coup on renvoie rien parce que voilà
+        return None
 
 def modulo(x,y):
     return x%y
 
-def calcul_r_par_s(salaire_h, h_jo, jo_an) :
-    return ((salaire_h*h_jo) * jo_an) / 3600
+def calculSalaireParSeconde(salaireHoraire, heureParJourOuvrable, jourOuvrable) :
+    salaireAnnuel = salaireHoraire * heureParJourOuvrable * jourOuvrable #calcul du salaire annuel
+    nbSecondeParAn = 365 * 24 * 60 * 60 #calcul du nombre de secondes par an (pour une année non bissextile)
+    return salaireAnnuel / nbSecondeParAn #calcul du revenu par seconde obtenu en divisant le salaire annuel par le nombre de secondes dans une année
 
+def salaire_net(salaireBrut, fonctionPublique=False):
+    #est-ce que la personne est dans la fonction publique ?
+    if fonctionPublique : 
+        #si oui, son salaire net est égal à 85% du salaire brut, car 15% sont partis aux charges sociales et aux impôts
+        return salaireBrut * 0.85 
+    #sinon ? si le if ne s'active pas et qu'il n'y a pas déjà le précédent return, c'est que la personne n'est pas du secteur public, donc on renvoie bien la ligne qui suit dans tous les autres cas
+    #le salaire net est égal à 77% du salaire brut, car les salariés qui ne sont pas de la fonction publique doivent 23% de ce dernier aux charges sociales et aux impôts
+    return salaireBrut * 0.77
+
+"""
 DEBUT
  DEFINIR add(x, y) :
     RENVOYER x + y
@@ -34,3 +53,4 @@ DEBUT
 
 
 FIN
+"""
